fbdd6926013466c0d3b8b9226d28ea839918b70c
蓑田 和麻 <k_minoda@r.recruit.co.jp>
2017-05-03 19:21:09 +0900

    test終了

--- test.py
+++ test.py
@@ -1,12 +1,39 @@
 # -*- coding: utf-8 -*-
+
 from human import HumanPlayer
 from bord import Board
 import numpy as np
 import chainerrl
+from dqn import QFunctions
+import chainer
+from randomAct import RandomActor
 
 human_player = HumanPlayer()
 b = Board()
-agent_p1 = chainerrl.agents.DoubleDQN.load("result_20000")
+ra = RandomActor(b)
+
+
+# 環境の次元数
+obs_size = 9
+# 行動の次元数
+n_actions = 9
+q_func = QFunctions(obs_size,n_actions)
+optimizer = chainer.optimizers.Adam(eps=1e-2)
+optimizer.setup(q_func)
+
+# 報酬の割引率
+gamma = 0.95
+# epsilon-greedyを使ってたまに冒険
+explorer = chainerrl.explorers.LinearDecayEpsilonGreedy(
+    start_epsilon=1.0, end_epsilon=0.3, decay_steps=50000, random_action_func=ra.random_action
+)
+replay_buffer = chainerrl.replay_buffer.ReplayBuffer(capacity=10**6)
+# agentの生成
+agent_p1 = chainerrl.agents.DoubleDQN(
+    q_func, optimizer, replay_buffer, gamma, explorer, replay_start_size=500, update_frequency=1,
+    target_update_frequency=100
+)
+agent_p1.load("result_20000")
 
 for i in range(10):
     b.reset()
